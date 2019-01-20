b235c16d3d6bd47723257b18be4fa7103617b99c
jeuxjeux20 Y <jeuxjeux20@outlook.fr>
2019-01-20 23:35:08 +0100

    Changed version

--- VeryHotKeys.WPF/Properties/AssemblyInfo.cs
+++ VeryHotKeys.WPF/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("VeryHotKeys")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An easy way to setup global key shortcuts on your WPF app.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VeryHotKeys")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
